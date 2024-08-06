719d65cffad2ab0d34a6ceec2eb8ab2eab48eeba
Gustavo Henrique <gustavohgustavo@hotmail.com>
2024-08-06 16:01:55 -0300

    Multipos de 3 ou 5.

--- desafiosaczg/src/desafiosaczg/Multiples3of5.java
+++ desafiosaczg/src/desafiosaczg/Multiples3of5.java
@@ -2,10 +2,9 @@ package desafiosaczg;
 
 public class Multiples3of5 {
 	public static void main(String[] args) {
-		int limit = 1000;
 		int sum = 0;
 
-		for (int i = 1; i < limit; i++) {
+		for (int i = 1; i < 1000; i++) {
 			if (i % 3 == 0 || i % 5 == 0) {
 				sum += i;
 			}
